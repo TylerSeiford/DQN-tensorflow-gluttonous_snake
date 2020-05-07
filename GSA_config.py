2692fff6453747a72af2f833ba17b6f5219ecf6a
TylerSeiford <50031864+TylerSeiford@users.noreply.github.com>
2020-05-07 13:08:24 -0500

    Created GSA

---
+++ GSA_config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class Config:
+    explore = 4000000
+    memory_size = 10000
+    batch_size = 256
+    n_actions = 4
+    g = 6.67e-11
+    rand = 2.0
+    model_file = './model/gsa'
